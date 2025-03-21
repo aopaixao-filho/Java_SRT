c3c6b8ead694a7f191c182f493e08cb7cc0baf26
aopaixao-filho <aopaixao.filho@outlook.com>
2025-03-21 20:59:34 -0300

    aula10

---
+++ aula10/src/aula10/exemplos.java
@@ -0,0 +1,17 @@
+package aula10;
+
+public class exemplos {
+
+	public static void main(String[] args) {
+		String[] vetor = new String[5];
+		
+		vetor[0] = "Amarelo";
+		vetor[1] = "Verde";
+		vetor[2] = "Laranja";
+		vetor[3] = "Roxo";
+		vetor[4] = "Azul";
+		
+		System.out.println(vetor[0]);
+	}
+
+}
